a970c85deeef8f47330fa7587a1f040433300fa3
Renan Joppert <renanjoppert@outlook.com>
2021-03-23 22:52:33 -0300

    Aula 6

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-      child: Center(
-          child: Text(
-    'Fluterando 2',
-    textDirection: TextDirection.ltr,
-    style: TextStyle(color: Colors.white, fontSize: 50.0),
-  ))));
+  runApp(AppWidget(title: 'Fluterando 3'));
+}
+
+class AppWidget extends StatelessWidget {
+  // Criando parâmetros para o método do widget
+  final String title;
+  // configuração dos parâmetros
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  // build do widget
+  @override
+  Widget build(BuildContext context) {
+    // conteúdo do widget - Container
+    return Container(
+        // elemento filho - Center é um componente que centraliza elemento nos 2 axis
+        child: Center(
+            // elemento filho text
+            child: Text(
+      title,
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.white, fontSize: 50.0),
+    )));
+  }
 }
